fb3b70ccda0f2c83171ad631e504fde1ebe9ebf0
samirvithlani <samir.vithlani83955@gmail.com>
2023-10-28 17:43:07 +0530

    put,get,delete

--- src/main/java/com/controller/EmployeeController.java
+++ src/main/java/com/controller/EmployeeController.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +24,11 @@ public class EmployeeController {
 	@Autowired
 	EmployeeDao employeeDao;
 
-	@GetMapping("/employee")
-	public List<EmployeeBean> getAllEmployees() {
-
-		return employeeList;
-	}
+//	@GetMapping("/employee")
+//	public List<EmployeeBean> getAllEmployees() {
+//
+//		return employeeList;
+//	}
 
 	@PostMapping("/employee")
 	public String addEmployee(@RequestBody EmployeeBean employeeBean) {
@@ -47,6 +50,48 @@ public class EmployeeController {
 		}
 
 		return "employee not addedd";
+	}
+
+	@PutMapping(value = "/updateemployee/{id}")
+	public String updateEmployee(@PathVariable("id") int id, @RequestBody EmployeeBean employeeBean) {
+		int res = employeeDao.updateEmployee(id, employeeBean);
+		if (res > 0) {
+
+			return "employee updated...";
+		}
+		return "employee not updated..";
+
+	}
+
+	@DeleteMapping(value = "/deleteemployee/{id}")
+	public String deleteEmployee(@PathVariable("id") int id) {
+
+		int res = employeeDao.deleteEmployee(id);
+		if (res > 0) {
+
+			return "employee deleted";
+		}
+		return "employee not deleted";
+	}
+
+	@GetMapping(value = "/employees")
+	public List<EmployeeBean> getAllEmployees() {
+
+		List<EmployeeBean> employees = employeeDao.getAllEmployees();
+
+		return employees;
+
+	}
+
+	@GetMapping(value = "/employee/{id}")
+	public EmployeeBean getEmpById(@PathVariable("id") int id) {
+
+		EmployeeBean employeeBean = employeeDao.getEMployeeById(id);
+		if (employeeBean != null) {
+			System.out.println("..");
+			return employeeBean;
+		}
+		return null;
 
 	}
 
--- src/main/java/com/dao/EmployeeDao.java
+++ src/main/java/com/dao/EmployeeDao.java
@@ -1,7 +1,12 @@
 package com.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.bean.EmployeeBean;
@@ -17,5 +22,47 @@ public class EmployeeDao {
 		return jdbcTemplate.update("insert into employee (ename,eage,esalary)values(?,?,?)", employeeBean.geteName(),
 				employeeBean.geteAge(), employeeBean.geteSalary());
 
+	}
+
+	public int updateEmployee(int eId, EmployeeBean employeeBean) {
+
+		return jdbcTemplate.update("update employee set ename=?,eage=?,esalary=? where eid = ?",
+				employeeBean.geteName(), employeeBean.geteAge(), employeeBean.geteSalary(), eId);
+	}
+
+	public int deleteEmployee(int eId) {
+
+		return jdbcTemplate.update("delete from employee where eId = ?", eId);
+	}
+
+	private class EmployeeMapper implements RowMapper<EmployeeBean> {
+
+		@Override
+		public EmployeeBean mapRow(ResultSet rs, int rowNum) throws SQLException {
+			EmployeeBean employeeBean = new EmployeeBean();
+			employeeBean.seteId(rs.getInt("eId"));
+			employeeBean.seteName(rs.getString("ename"));
+			employeeBean.seteAge(rs.getInt("eAge"));
+			employeeBean.seteSalary(rs.getDouble("eSalary"));
+			return employeeBean;
+		}
+
+	}
+
+	public List<EmployeeBean> getAllEmployees() {
+
+		return jdbcTemplate.query("select * from employee", new EmployeeMapper());
+
+	}
+
+	public EmployeeBean getEMployeeById(int eId) {
+		
+		
+		try {
+			return jdbcTemplate.queryForObject("select * from employee where eid = "+eId+"", new EmployeeMapper());
+		}
+		catch (Exception e) {
+			return null;
+		}
 	}
 }
